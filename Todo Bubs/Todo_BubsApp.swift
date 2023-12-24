1b81f15a78333a0aa66f1b9f16ae52c5a1b3f2d7
Ammar Farhan Mohamad Rizam <ammar_22006911@utp.edu.my>
2023-12-24 22:47:20 +0800

    Initial Commit

---
+++ Todo Bubs/Todo_BubsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Todo_BubsApp.swift
+//  Todo Bubs
+//
+//  Created by Ammar Farhan Mohamad Rizam on 24/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct Todo_BubsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
